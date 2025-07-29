49d7fc84b091eb58ff2ff7b5e914671780b137dc
Rusiru-Tharusha. <hello.tharusha1@gmail.com>
2025-07-29 23:14:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "3J9xTCQR#7tNDjcLEVERub0V0PBeOV1SiIeGhxcRQ4Rej4gNMY88",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:iSHJUNaEpUUqSRnBDypCALxpajdIcyBm@gondola.proxy.rlwy.net:43532",
+  OWNER_NUM: process.env.OWNER_NUM || "94714774537",
 };
